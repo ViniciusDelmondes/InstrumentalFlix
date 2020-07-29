886e6cb5e4997c1efb081a0caa8f510d3cc3d3dc
Vinicius Delmondes <viniciusdelmo@hotmail.com>
2020-07-29 00:17:54 -0300

    InstrumentalFlix

--- instrumentalflix/src/index.js
+++ instrumentalflix/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home/index'
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import CadastroVideo from './pages/cadastro/video/index'
+import CadastroCategoria from './pages/categoria/index'
+
+
+const Pagina404 = () => {
+  return (
+  <div>
+    <h1> Ops! Algo de errado não está certo!</h1>
+  </div>
+  )
+}
+  
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+      <Route component={Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
